65b3956c3006d86c2b31ab2862c00c62863875fd
MysticSlime <jorenjacob098@gmail.com>
2026-02-20 02:13:26 +0800

    ended before "OffsetPageTable"

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@
 #![reexport_test_harness_main = "test_main"]
 
 use bootloader::{BootInfo, entry_point};
-use catmeow_os::println;
+use catmeow_os::{memory::translate_addr, println};
 use core::panic::PanicInfo;
 
 //#[unsafe(no_mangle)]
@@ -19,22 +19,17 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     use x86_64::VirtAddr;
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
-    for (i, entry) in l4_table.iter().enumerate() {
-        if !entry.is_unused() {
-            println!("L4 Entry: {}: {:?}", i, entry);
+    let addresses = [
+        0xb8000,
+        0x201008,
+        0x0100_0020_1a10,
+        boot_info.physical_memory_offset,
+    ];
 
-            let phys = entry.frame().unwrap().start_address();
-            let virt = phys.as_u64() + boot_info.physical_memory_offset;
-            let ptr = VirtAddr::new(virt).as_mut_ptr();
-            let l3_table: &PageTable = unsafe { &*ptr };
-            for (i, entry) in l3_table.iter().enumerate() {
-                if !entry.is_unused() {
-                    println!("L3 Entry: {}: {:?}", i, entry);
-                }
-            }
-
-        }
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        println!("{:?} -> {:?}", virt, phys);
     }
 
     /*
--- src/memory.rs
+++ src/memory.rs
@@ -1,3 +1,4 @@
+
 use x86_64::{PhysAddr, VirtAddr, structures::paging::{PageTable, page_table}};
 
 
@@ -12,7 +13,6 @@ pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static
 }
 
 
-use x86_64::PhysAddr;
 
 
 pub unsafe fn translate_addr(addr: VirtAddr, physical_memory_offset: VirtAddr) -> Option<PhysAddr> {
@@ -24,5 +24,24 @@ fn translate_addr_inner(addr: VirtAddr, physical_memory_offset: VirtAddr) -> Opt
     use x86_64::structures::paging::page_table::FrameError;
     use x86_64::registers::control::Cr3;
 
-    let (, _)
+    let (level_4_table_frame, _) = Cr3::read();
+    let table_indexes = [
+        addr.p4_index(), addr.p3_index(), addr.p2_index(), addr.p1_index()
+    ];
+    let mut frame = level_4_table_frame;
+
+    for &index in &table_indexes {
+        let virt = physical_memory_offset + frame.start_address().as_u64();
+        let table_ptr: *const PageTable = virt.as_ptr();
+        let table = unsafe { &*table_ptr};
+        let entry = &table[index];
+        frame = match entry.frame() {
+            Ok(frame) => frame,
+            Err(FrameError::FrameNotPresent) => return None,
+            Err(FrameError::HugeFrame) => panic!("huge pages not supported"),
+        }
+
+    }
+
+    Some(frame.start_address() + u64::from(addr.page_offset()))
 }
