62bfc135d254491559325138c5fe90d799aa3fef
MysticSlime <jorenjacob098@gmail.com>
2026-03-23 22:04:27 +0800

    fixed idt handlers

--- kernel/src/idt.rs
+++ kernel/src/idt.rs
@@ -2,7 +2,7 @@ use crate::{serial_println, terminal_println};
 use core::arch::asm;
 use lazy_static::lazy_static;
 use x86_64::{
-    instructions::interrupts::int3,
+    instructions::{hlt, interrupts::int3},
     registers::control::Cr2,
     structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode},
 };
@@ -11,13 +11,16 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.divide_error.set_handler_fn(divide_error);
-        idt.debug.set_handler_fn(handler);
-        idt.non_maskable_interrupt.set_handler_fn(handler);
+        idt.debug.set_handler_fn(debug);
+        idt.non_maskable_interrupt
+            .set_handler_fn(non_maskable_interrupt);
         idt.breakpoint.set_handler_fn(breakpoint_handler);
-        idt.overflow.set_handler_fn(handler);
-        idt.bound_range_exceeded.set_handler_fn(handler);
-        idt.invalid_opcode.set_handler_fn(handler);
-        idt.device_not_available.set_handler_fn(handler);
+        idt.overflow.set_handler_fn(overflow);
+        idt.bound_range_exceeded
+            .set_handler_fn(bound_range_exceeded);
+        idt.invalid_opcode.set_handler_fn(invalid_opcode);
+        idt.device_not_available
+            .set_handler_fn(device_not_available);
         idt.double_fault.set_handler_fn(double_fault);
         idt.invalid_tss.set_handler_fn(invalid_tss);
         idt.segment_not_present.set_handler_fn(segment_not_present);
@@ -25,22 +28,59 @@ lazy_static! {
         idt.general_protection_fault
             .set_handler_fn(general_protection_fault);
         idt.page_fault.set_handler_fn(page_fault);
-        idt.x87_floating_point.set_handler_fn(handler);
+        idt.x87_floating_point.set_handler_fn(x87_floating_point);
         idt.alignment_check.set_handler_fn(alignment_check);
         idt.machine_check.set_handler_fn(machine_check);
-        idt.simd_floating_point.set_handler_fn(handler);
-        idt.virtualization.set_handler_fn(handler);
+        idt.simd_floating_point.set_handler_fn(simd_floating_point);
+        idt.virtualization.set_handler_fn(virtualization);
         idt.cp_protection_exception
             .set_handler_fn(cp_protection_exception);
-        idt.hv_injection_exception.set_handler_fn(handler);
+        idt.hv_injection_exception
+            .set_handler_fn(hv_injection_exception);
         idt.vmm_communication_exception
             .set_handler_fn(vmm_communication_exception);
         idt.security_exception.set_handler_fn(security_exception);
+        idt[128].set_handler_fn(test);
         idt
     };
 }
-extern "x86-interrupt" fn handler(stack_frame: InterruptStackFrame) {
-    serial_println!("{:#?}", stack_frame);
+
+extern "x86-interrupt" fn debug(stack_frame: InterruptStackFrame) {
+    serial_println!("debug: {:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn non_maskable_interrupt(stack_frame: InterruptStackFrame) {
+    serial_println!("non_maskable_interrupt: {:#?}", stack_frame);
+}
+extern "x86-interrupt" fn overflow(stack_frame: InterruptStackFrame) {
+    serial_println!("overflow: {:#?}", stack_frame);
+}
+extern "x86-interrupt" fn bound_range_exceeded(stack_frame: InterruptStackFrame) {
+    serial_println!("bound_range_exceeded: {:#?}", stack_frame);
+}
+extern "x86-interrupt" fn invalid_opcode(stack_frame: InterruptStackFrame) {
+    serial_println!("invalid_opcode: {:#?}", stack_frame);
+    loop {}
+}
+extern "x86-interrupt" fn device_not_available(stack_frame: InterruptStackFrame) {
+    serial_println!("device_not_available: {:#?}", stack_frame);
+}
+extern "x86-interrupt" fn x87_floating_point(stack_frame: InterruptStackFrame) {
+    serial_println!("x87_floating_point: {:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn simd_floating_point(stack_frame: InterruptStackFrame) {
+    serial_println!("simd_floating_point: {:#?}", stack_frame);
+}
+extern "x86-interrupt" fn virtualization(stack_frame: InterruptStackFrame) {
+    serial_println!("virtualization: {:#?}", stack_frame);
+}
+extern "x86-interrupt" fn hv_injection_exception(stack_frame: InterruptStackFrame) {
+    serial_println!("hv_injection_exception: {:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn test(stack_frame: InterruptStackFrame) {
+    serial_println!("this was called: {:#?}", stack_frame);
 }
 
 extern "x86-interrupt" fn segment_not_present(stack_frame: InterruptStackFrame, err: u64) {
@@ -53,6 +93,9 @@ extern "x86-interrupt" fn stack_segment_fault(stack_frame: InterruptStackFrame, 
 
 extern "x86-interrupt" fn general_protection_fault(stack_frame: InterruptStackFrame, err: u64) {
     serial_println!("general_protection_fault: {:#?}, {:#?}", stack_frame, err);
+    loop {
+        hlt();
+    }
 }
 
 extern "x86-interrupt" fn alignment_check(stack_frame: InterruptStackFrame, err: u64) {
