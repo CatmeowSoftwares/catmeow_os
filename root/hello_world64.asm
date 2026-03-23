e5dfd7d5297a04bb73e98a9abb21033f03e8ea13
MysticSlime <jorenjacob098@gmail.com>
2026-03-23 22:05:02 +0800

    Create hello_world64.asm

---
+++ root/hello_world64.asm
@@ -0,0 +1,21 @@
+
+bits 64
+global _start
+
+section .text
+
+_start:
+  mov rax, 1        ; write(
+  mov rdi, 1        ;   STDOUT_FILENO,
+  mov rsi, msg      ;   "Hello, world!\n",
+  mov rdx, msglen   ;   sizeof("Hello, world!\n")
+  syscall           ; );
+
+  ;mov rax, 60       ; exit(
+  ;mov rdi, 0        ;   EXIT_SUCCESS
+  ;syscall           ; );
+  jmp _start
+
+section .rodata
+  msg: db "Hello, world!", 10
+  msglen: equ $ - msg
