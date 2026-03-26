4c9efb380375301233b41e17fe264b0f19c6d73b
MysticSlime <jorenjacob098@gmail.com>
2026-03-26 23:22:18 +0800

    removed unnecessary code in gdt

--- kernel/src/gdt.rs
+++ kernel/src/gdt.rs
@@ -1,7 +1,6 @@
 use core::{
     arch::{asm, global_asm},
     cell::SyncUnsafeCell,
-    f64::consts::PI,
 };
 
 use crate::serial_println;
@@ -96,52 +95,22 @@ impl GlobalDescriptorTableRegister {
     }
 }
 
-struct Descriptor {
-    base: u32,
-    limit: u64,
-    access_byte: u8,
-    flags: u8,
-}
-
-#[repr(packed)]
-struct GDTDesc {
-    limit0_15: u16,
-    base0_15: u16,
-    base16_23: u8,
-    access: u8,
-    limit16_19: u8,
-    other: u8,
-    flags: u8,
-    base24_31: u8,
-}
 static GDTR: SyncUnsafeCell<GlobalDescriptorTableRegister> =
     SyncUnsafeCell::new(GlobalDescriptorTableRegister::new());
 static GDT: SyncUnsafeCell<[u64; 6]> = SyncUnsafeCell::new([0u64; 6]);
 
 pub fn init_gdt() {
-    /*
-    create_descriptor(base, limit, flag);
-    create_descriptor(base, limit, flag);
-    create_descriptor(base, limit, flag);
-    create_descriptor(base, limit, flag);
-    create_descriptor(base, limit, flag);
-    create_descriptor(base, limit, flag);
-     */
     let gdt = unsafe { &mut *GDT.get() };
     gdt[0] = create_descriptor(0, 0, 0);
-    gdt[1] = create_descriptor(0, 0x000FFFFF, (GDT_CODE_PL0));
-    gdt[2] = create_descriptor(0, 0x000FFFFF, (GDT_DATA_PL0));
-    gdt[3] = create_descriptor(0, 0x000FFFFF, (GDT_CODE_PL3));
-    gdt[4] = create_descriptor(0, 0x000FFFFF, (GDT_DATA_PL3));
+    gdt[1] = create_descriptor(0, 0x000FFFFF, GDT_CODE_PL0);
+    gdt[2] = create_descriptor(0, 0x000FFFFF, GDT_DATA_PL0);
+    gdt[3] = create_descriptor(0, 0x000FFFFF, GDT_CODE_PL3);
+    gdt[4] = create_descriptor(0, 0x000FFFFF, GDT_DATA_PL3);
 
-    let gdtr = GDTR.get();
-    unsafe {
-        (*gdtr).limit = (size_of_val(gdt) - 1) as u16;
-        (*gdtr).base = gdt.as_ptr() as u64;
-    }
-    unsafe {
-        lgdt(gdtr.as_mut_unchecked());
-    }
+    let gdtr = unsafe { &mut *GDTR.get() };
+    gdtr.limit = (size_of_val(gdt) - 1) as u16;
+    gdtr.base = gdt.as_ptr() as u64;
+    lgdt(gdtr);
     reload_segments();
     serial_println!("GDT INITIALIZED")
 }
@@ -165,94 +134,13 @@ fn reload_segments() {
         );
     }
 }
-/*
-global_asm!(
-    r#"
-    .globl reload_cs
-    reload_cs:
-       #; Reload data segment registers
-       MOV   AX, 0x10 #; 0x10 is a stand-in for your data segment
-       MOV   DS, AX
-       MOV   ES, AX
-       MOV   FS, AX
-       MOV   GS, AX
-       MOV   SS, AX
-       RET
-    "#
-);
-global_asm!(
-    r#"
-    .globl reload_segments
-    reload_segments:
-       #; Reload CS register:
-       PUSH 0x08                 #; Push code segment to stack, 0x08 is a stand-in for your code segment
-       LEA RAX, [rel {rcs}] #; Load address of .reload_CS into RAX
-       PUSH RAX                  #; Push this value to the stack
-       RETFQ                     #; Perform a far return, RETFQ or LRETQ depending on syntax
-    "#, rcs = sym reload_cs
-);
 
-unsafe extern "C" {
-    fn reload_cs();
-    fn reload_segments();
-}
- */
-fn reload_cs1() {
-    unsafe {
-        asm!(
-            "
-            mov ax 0x10
-            mov ds, ax
-            mov es, ax
-            mov fs, ax
-            mov gs, ax
-            mov ss, ax
-
-
-            "
-        );
-    }
-}
 fn lgdt(gdt: *const GlobalDescriptorTableRegister) {
     unsafe { asm!("lgdt [{}]", in(reg) gdt, options(readonly, nostack, preserves_flags)) }
 }
-struct Segment {
-    base: u64,
-    limit: u64,
-}
-
-struct NullDescriptor {
-    base: u64,
-    limit: u64,
-}
-struct KernelCodeSegment {}
-struct KernelDataSegment {}
-struct UserDataSegment {}
-struct UserCodeSegment {}
-struct TaskStateSegment {}
-
-fn encode_gdt_entry(target: *mut u8, source: Descriptor) {
-    if source.limit > 0xFFFFF {
-        panic!("GDT cannot encode limits larger than 0xFFFFF");
-    }
-    unsafe {
-        *target.add(0) = (source.limit & 0xff) as u8;
-        *target.add(1) = ((source.limit >> 8) & 0xff) as u8;
-        *target.add(6) = ((source.limit >> 16) & 0xff) as u8;
-
-        *target.add(2) = (source.base & 0xff) as u8;
-        *target.add(3) = ((source.base >> 8) & 0xff) as u8;
-        *target.add(4) = ((source.base >> 16) & 0xff) as u8;
-        *target.add(7) = ((source.base >> 24) & 0xff) as u8;
-
-        *target.add(5) = source.access_byte;
-
-        *target.add(6) |= source.flags << 4;
-    }
-}
 
 pub fn create_descriptor(base: u32, limit: u32, flag: u16) -> u64 {
-    let mut descriptor = 0u64;
+    let mut descriptor;
 
     descriptor = (limit & 0x000F0000) as u64;
 
@@ -264,6 +152,6 @@ pub fn create_descriptor(base: u32, limit: u32, flag: u16) -> u64 {
     descriptor |= (base << 16) as u64;
     descriptor |= (limit & 0x0000FFFF) as u64;
 
-    serial_println!("0x{:016x}", descriptor);
+    //serial_println!("0x{:016x}", descriptor);
     descriptor
 }
