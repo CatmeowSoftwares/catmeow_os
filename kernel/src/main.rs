69997cb4fe00f6ff9dc784f82a27aaef08f60615
MysticSlime <jorenjacob098@gmail.com>
2026-03-27 23:47:14 +0800

    some changes in main

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -3,8 +3,17 @@
 
 use core::arch::asm;
 
+use catmeow_os::gdt::init_gdt;
+use catmeow_os::idt::init_idt;
+use catmeow_os::memory::{self, init_memory};
+use catmeow_os::serial::init_serial;
+use catmeow_os::serial_println;
 use limine::BaseRevision;
-use limine::request::{FramebufferRequest, RequestsEndMarker, RequestsStartMarker};
+use limine::memory_map::EntryType;
+use limine::paging::Mode;
+use limine::request::{
+    FramebufferRequest, MemoryMapRequest, PagingModeRequest, RequestsEndMarker, RequestsStartMarker,
+};
 
 /// Sets the base revision to the latest revision supported by the crate.
 /// See specification for further info.
@@ -17,6 +26,14 @@ static BASE_REVISION: BaseRevision = BaseRevision::new();
 #[used]
 #[unsafe(link_section = ".requests")]
 static FRAMEBUFFER_REQUEST: FramebufferRequest = FramebufferRequest::new();
+
+#[used]
+#[unsafe(link_section = ".requests")]
+static MEMORY_MAP_REQUEST: MemoryMapRequest = MemoryMapRequest::new();
+
+#[used]
+#[unsafe(link_section = ".requests")]
+static PAGING_MODE_REQUEST: PagingModeRequest = PagingModeRequest::new();
 
 /// Define the stand and end markers for Limine requests.
 #[used]
@@ -31,7 +48,10 @@ unsafe extern "C" fn kmain() -> ! {
     // All limine requests must also be referenced in a called function, otherwise they may be
     // removed by the linker.
     assert!(BASE_REVISION.is_supported());
-
+    init_serial();
+    serial_println!("start of os");
+    init_gdt();
+    init_idt();
     if let Some(framebuffer_response) = FRAMEBUFFER_REQUEST.get_response() {
         if let Some(framebuffer) = framebuffer_response.framebuffers().next() {
             for i in 0..100_u64 {
@@ -49,13 +69,34 @@ unsafe extern "C" fn kmain() -> ! {
                 };
             }
         }
+        if let Some(memory_map_response) = MEMORY_MAP_REQUEST.get_response() {
+            let entries = memory_map_response.entries();
+            init_memory(entries);
+        }
+        if let Some(paging_mode_response) = PAGING_MODE_REQUEST.get_response() {
+            let mode = paging_mode_response.mode();
+            if mode == Mode::DEFAULT {
+                serial_println!("default paging");
+            }
+            if mode == Mode::FIVE_LEVEL {
+                serial_println!("five level paging");
+            }
+            if mode == Mode::FOUR_LEVEL {
+                serial_println!("four level paging");
+            }
+            if mode == Mode::MIN {
+                serial_println!("min paging paging")
+            }
+        }
     }
-
+    serial_println!("end");
     hcf();
 }
 
+#[cfg(not(test))]
 #[panic_handler]
-fn rust_panic(_info: &core::panic::PanicInfo) -> ! {
+fn rust_panic(info: &core::panic::PanicInfo) -> ! {
+    serial_println!("{}", info);
     hcf();
 }
 
